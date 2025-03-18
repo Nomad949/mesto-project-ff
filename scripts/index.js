ea9317bb81392c9543460360bc330a6d21d5d013
Maxim Sultan <aldair94@mail.ru>
2025-03-19 00:04:30 +0200

    added cards on list

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placeCards = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard (initialCards, removeCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const deleteButton = card.querySelector('.card__delete-button');
 
+    cardImage.src = initialCards.link;
+    cardImage.alt = initialCards.name;
+    cardTitle.textContent = initialCards.name;
+
+    deleteButton.addEventListener('click', () => {
+        removeCard(card);
+    })
+
+    return placeCards.append(card);
+}
 // @todo: Функция удаления карточки
-
+function removeCard () {
+    
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => createCard(card));
+/**/
