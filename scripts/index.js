5da137be3f06be82a5037765a0df85686e627aea
Nomad949 <aldair94@mail.ru>
2025-03-19 13:42:47 +0200

    added function Delete

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,28 @@
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placeCards = document.querySelector('.places__list');
+const deleteButton = cardTemplate.querySelector('.card__delete-button');
 // @todo: Функция создания карточки
-function createCard (initialCards, removeCard) {
+function createCard (initialCards, deleteCard) {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = card.querySelector('.card__image');
     const cardTitle = card.querySelector('.card__title');
-    const deleteButton = card.querySelector('.card__delete-button');
-
+    
     cardImage.src = initialCards.link;
     cardImage.alt = initialCards.name;
     cardTitle.textContent = initialCards.name;
 
-    deleteButton.addEventListener('click', () => {
-        removeCard(card);
-    })
+    deleteButton.addEventListener('click', deleteCard);
 
     return placeCards.append(card);
 }
+
 // @todo: Функция удаления карточки
-function removeCard () {
-    
+function deleteCard() {  
+    const listItem = deleteButton.closest('.card');
+    listItem.remove();
 }
+
 // @todo: Вывести карточки на страницу
-initialCards.forEach(card => createCard(card));
-/**/
+initialCards.forEach(card => createCard(card, deleteCard));
+
